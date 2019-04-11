c7494584ad88b1a1551e80e0ad0365625a5313f5
chikuan <you@example.com>
2019-04-11 14:53:53 -0400

    update re3-tracker

--- tracker/re3_tracker.py
+++ tracker/re3_tracker.py
@@ -2,15 +2,18 @@ import cv2
 import numpy as np
 import torch
 import time
+import argparse
 
 import sys
+import os
 import os.path
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.path.pardir)))
 from tracker.network import Re3Net
 import utils.bb_util as bb_util
 import utils.im_util as im_util
-from training import get_sequence
+import utils.drawing as drawing
+
 from constants import CROP_PAD
 from constants import CROP_SIZE
 
@@ -18,7 +21,10 @@ class Re3Tracker(object):
     def __init__(self, model_path, device):
         self.net = Re3Net().to(device)
         if model_path is not None:
-            self.net.load_state_dict(torch.load(model_path))
+            if device.type == "cpu":
+                self.net.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
+            else:
+                self.net.load_state_dict(torch.load(model_path))
         self.net.eval()
         self.device = device
         self.tracked_data = {}
@@ -40,7 +46,7 @@ class Re3Tracker(object):
         cropped_input1, _ = im_util.get_cropped_input(image, past_bbox, CROP_PAD, CROP_SIZE)
 
         network_input = np.stack((cropped_input0.transpose(2,0,1),cropped_input1.transpose(2,0,1)))
-        network_input = network_input.reshape((1,2,3,CROP_SIZE, CROP_SIZE))
+        # network_input = network_input.reshape((2,3,CROP_SIZE, CROP_SIZE))
         network_input = torch.tensor(network_input, dtype = torch.float)    
         
         with torch.no_grad():
@@ -53,10 +59,10 @@ class Re3Tracker(object):
             state2_h = lstm_state[1][0].clone()
             state2_c = lstm_state[1][1].clone()
             initial_state = ((state1_h, state1_c), (state2_h, state2_c))
-        
+            
         prev_image = image
         # ***** padding = 1 or 2
-        output_bbox = bb_util.from_crop_coordinate_system(network_output.data.numpy(), past_bbox_padded, 1, 1)
+        output_bbox = bb_util.from_crop_coordinate_system(network_output.data.numpy()/10, past_bbox_padded, CROP_PAD, 1)
 
         # Reset state
         if forward_count > 0 and forward_count % 32 == 0:
@@ -77,10 +83,22 @@ class Re3Tracker(object):
         self.tracked_data = {}
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Tracking with Re3Net.')
+    parser.add_argument('-p', '--path', action='store', type=str)
+    parser.add_argument('-b', '--bbox', action='store', type=str)
+    args = parser.parse_args()
+    PATH = args.path
+    bbox = args.bbox
+    bbox = np.array([int(b) for b in bbox.split()])
+    paths = [PATH + f for f in os.listdir(PATH)]
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    net = Re3Net().to(device)
-    net.load_state_dict(torch.load('checkpoint.pth'))
-    net.eval()
-    tracker = Re3Tracker(net, device)
-
-
+    tracker = Re3Tracker('checkpoint.pth', device)
+    print('start_bbox', bbox)
+    for i in range(len(paths)):
+        img = cv2.imread(paths[i])
+        output = tracker.track(1,img, bbox=bbox)
+        print('output', output.astype('int32'))
+        patch = drawing.drawRect(img, output, 1, (255,0,0))
+        cv2.imwrite('test_results/track_result_%05d.png'%(i), patch)
+        bbox = None
+    
