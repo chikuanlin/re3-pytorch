19549d54fb6728fbccc4c3cc95c96e6d725829f4
chikuan <you@example.com>
2019-04-14 12:05:10 -0400

    update training

--- training/training_v2.py
+++ training/training_v2.py
@@ -15,7 +15,6 @@ import os.path
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(__file__), os.path.pardir)))
 from tracker.network import Re3Net
-import get_sequence
 import get_rand_sequence
 
 def train(trainloader, net, criterion, optimizer, device, num_unrolls = 2):
@@ -50,9 +49,8 @@ def main(args):
     max_steps = args.max_steps
     learning_rate = args.learning_rate
     batch_size = args.batch_size
+    use_net_prob = args.use_net_prob
     PATH = args.model_path
-
-#     start_line = np.loadtxt('start_line.txt', dtype=int)
 
     # Re3Net Set up
     net = Re3Net().to(device)
@@ -61,10 +59,10 @@ def main(args):
         net.eval()
     criterion = nn.L1Loss()
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=5e-4)
-#     dataset_train = get_sequence.Dataset(32, start_line = start_line[0])
-#     dataset_val = get_sequence.Dataset(32, 'val', start_line = start_line[1])
-    dataset_train = get_rand_sequence.Dataset(num_unrolls)
-    dataset_val = get_rand_sequence.Dataset(num_unrolls, 'val')
+
+    dataset_train = get_rand_sequence.Dataset(net, num_unrolls, USE_NETWORK_PROB = use_net_prob)
+    dataset_val = get_rand_sequence.Dataset(net, num_unrolls, mode = 'val')
+    
     print('Start Training: ')
     for epoch in range(1, max_steps+1):
         train_X = []
@@ -95,11 +93,9 @@ def main(args):
 
         print('[Epoch %d / %d] train_loss: %.5f val_loss: %.5f video id: %d' % (epoch, max_steps, train_loss, val_loss, dataset_train.video_idx))
         
-        if epoch % 10 == 0:
+        if epoch % 50 == 0:
             torch.save(net.state_dict(), DESTINATION)
-#         start_line[0] = dataset_train.cur_line
-#         start_line[1] = dataset_val.cur_line
-#         np.savetxt('start_line.txt', start_line, fmt = '%d')
+
 
 
 
@@ -109,6 +105,7 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--max_steps', action='store', type=int, default=100)
     parser.add_argument('-b', '--batch_size', action='store', type=int, default=64)
     parser.add_argument('-l', '--learning_rate', action='store', type=float, default=1e-5)
+    parser.add_argument('-u', '--use_net_prob', action='store', type=float, default=0)
     parser.add_argument('-p', '--model_path', action='store', type=str, default='checkpoint.pth')
     args = parser.parse_args()
     main(args)
