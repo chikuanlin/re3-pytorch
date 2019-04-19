9624923d7656850dba5f17256c68f96d14f0a13b
root <root@too-simple.asia-east1-a.c.certain-drake-236819.internal>
2019-04-19 02:27:59 +0000

    1w

--- train.sh
+++ train.sh
@@ -2,6 +2,6 @@
 # python3 training/training_v2.py -m 20000 -n 2 -u 0 -b 64 -l 1e-6 >> log
 # python3 training/training_v2.py -m 10000 -n 2 -u 0 -b 64 -l 1e-6 >> log
 # python3 training/training_v2.py -m 5000 -n 4 -u 0 -b 32 -l 1e-6 >> log
-python3 training/training_v2.py -m 10000 -n 4 -u 0 -b 32 -l 1e-6 >> log
-# python3 training/training_v2.py -m 40000 -n 8 -u 0 -b 16 -l 1e-6 >> log
+# python3 training/training_v2.py -m 10000 -n 4 -u 0 -b 32 -l 1e-6 >> log
+python3 training/training_v2.py -m 10000 -n 4 -u 0.1 -b 32 -l 1e-6 >> log
 # python3 training/training_v2.py -m 40000 -n 16 -u 0 -b 8 -l 1e-6 >> log
