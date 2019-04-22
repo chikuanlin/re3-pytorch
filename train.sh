90cffc7c20b13c3fae498e171542044517477fab
root <root@too-simple-too-fast.asia-east1-a.c.toosimple.internal>
2019-04-22 01:49:26 +0000

    -m 20000 -n 16 -u 0.2 -b 8 -l 5e-7

--- train.sh
+++ train.sh
@@ -5,18 +5,19 @@
 # python3 training/training_v2.py -m 10000 -n 4 -u 0 -b 32 -l 1e-6 >> log
 # python3 training/training_v2.py -m 10000 -n 4 -u 0.1 -b 32 -l 1e-6 >> log
 # python3 training/training_v2.py -m 15000 -n 4 -u 0.1 -b 32 -l 1e-6 >> log
-# git add .
-# git commit -m '-m 15000 -n 4 -u 0.1 -b 32 -l 1e-6'
-# git push https://pikelcw:pikelcw110@github.com/chikuanlin/re3-pytorch.git
-python3 training/training_v2.py -m 20000 -n 8 -u 0.1 -b 16 -l 1e-6 >> log
+# python3 training/training_v2.py -m 20000 -n 8 -u 0.1 -b 16 -l 1e-6 >> log
+# python3 training/training_v2.py -m 20000 -n 8 -u 0.2 -b 16 -l 1e-6 >> log
+
+python3 training/training_v2.py -m 20000 -n 16 -u 0.2 -b 8 -l 5e-7 >> log
 git add .
-git commit -m '-m 20000 -n 8 -u 0.1 -b 16 -l 1e-6'
+git commit -m '-m 20000 -n 16 -u 0.2 -b 8 -l 5e-7'
 git push https://pikelcw:pikelcw110@github.com/chikuanlin/re3-pytorch.git
 
-python3 training/training_v2.py -m 20000 -n 8 -u 0.2 -b 16 -l 1e-6 >> log
+python3 training/training_v2.py -m 20000 -n 16 -u 0.25 -b 8 -l 5e-7 >> log
 git add .
-git commit -m '-m 20000 -n 8 -u 0.2 -b 16 -l 1e-6'
+git commit -m '-m 20000 -n 16 -u 0.25 -b 8 -l 5e-7'
 git push https://pikelcw:pikelcw110@github.com/chikuanlin/re3-pytorch.git
+
 
 shutdown -h now
 
